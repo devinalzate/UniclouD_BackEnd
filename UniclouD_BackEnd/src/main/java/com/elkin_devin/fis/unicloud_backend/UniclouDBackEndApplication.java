be981f905e037c8b94a3d9df0747b6b7390da376
devinalzate <devinalzate2005@gmail.com>
2025-09-22 22:28:54 -0500

    inicializacion de proyecto

---
+++ UniclouD_BackEnd/src/main/java/com/elkin_devin/fis/unicloud_backend/UniclouDBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.elkin_devin.fis.unicloud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniclouDBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniclouDBackEndApplication.class, args);
+	}
+
+}
---
+++ UniclouD_BackEnd/src/test/java/com/elkin_devin/fis/unicloud_backend/UniclouDBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elkin_devin.fis.unicloud_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniclouDBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

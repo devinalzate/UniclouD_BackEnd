ab3d59cfd7408a35621f3ce598be0d34acf5c9da
devinalzate <devinalzate2005@gmail.com>
2025-11-26 14:54:09 -0500

    v1 de proyecto funcional

---
+++ UniclouD_BackEnd/src/main/java/com/elkin_devin/fis/unicloud_backend/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.elkin_devin.fis.unicloud_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/unicloud/**")
+                .allowedOrigins("http://localhost:5173")  // URL de tu React
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
--- UniclouD_BackEnd/src/main/java/com/elkin_devin/fis/unicloud_backend/controllers/ArticuloGeneralController.java
+++ UniclouD_BackEnd/src/main/java/com/elkin_devin/fis/unicloud_backend/controllers/ArticuloGeneralController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-
+//http://localhost:8081/unicloud/articulos-generales
 @RestController
 @RequestMapping("/unicloud/articulos-generales")
 @RequiredArgsConstructor
